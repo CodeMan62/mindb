60e9b567c285b4449b1d481eaca85a2fb91d8d9d
CodeMan62 <sharmahimanshu15082007@gmail.com>
2026-02-25 23:54:24 +0530

    repl(pager code is in dev branch)

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+fn main() -> Result<(), Box<dyn std::error::Error>>{
+    println!("Rust simple REPL. Type 'exit' to quit.");
+    run_repl();
+    Ok(())
+}
+
+
+fn run_repl(){
+        print!("▖  ▖▄▖▖ ▖▄ ▄
+▛▖▞▌▐ ▛▖▌▌▌▙▘
+▌▝ ▌▟▖▌▝▌▙▘▙▘
+             \n");
+    loop {
+        print!("mindb> ");
+        let mut input = String::new();
+        io::stdout().flush();
+        io::stdin().read_line(&mut input).expect("h");
+        let trimmed_input = input.trim();
+        if trimmed_input == "exit" {
+            break;
+        }
+        if trimmed_input.is_empty(){
+            continue;
+        }
+        println!("{}", trimmed_input);
+    }
+}
